e3be1be3d708bdfcb342e30f177efdd37d08a827
marmooo <marmooo@users.noreply.github.com>
2023-10-02 08:42:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# india-map-puzzle
+
+[A map puzzle of Indian states](https://marmooo.github.io/india-map-puzzle/).
+
+## Build
+
+```
+bash build.sh
+```
+
+## Related Projects
+
+- [Japan Map Puzzle](https://marmooo.github.io/japan-map-puzzle/)
+- [USA Map Puzzle](https://marmooo.github.io/usa-map-puzzle/)
+- [China Map Puzzle](https://marmooo.github.io/china-map-puzzle/)
+
+## Attribution
+
+- `map-original.svg` is cloned from
+  [Wikimedia](https://commons.wikimedia.org/wiki/File:India_location_map.svg)
+  ([SVG](https://upload.wikimedia.org/wikipedia/commons/d/dc/India_location_map.svg))
+  licensed under the CC-BY-SA-3.0
+- `src/map.svg` is modified from `map-original.svg` licensed under the
+  CC-BY-SA-4.0
+- `src/favicon/favicon.svg` is modified from
+  [mapsicon](https://github.com/djaiss/mapsicon)
---
+++ src/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "2023-10-01 00:00";
+const urlsToCache = [
+  "/india-map-puzzle/",
+  "/india-map-puzzle/en/",
+  "/india-map-puzzle/index.js",
+  "/india-map-puzzle/map.svg",
+  "/india-map-puzzle/data/en.lst",
+  "/india-map-puzzle/mp3/decision50.mp3",
+  "/india-map-puzzle/mp3/correct1.mp3",
+  "/india-map-puzzle/mp3/correct3.mp3",
+  "/india-map-puzzle/favicon/favicon.svg",
+  "https://cdn.jsdelivr.net/npm/fabric@5.3.0/dist/fabric.min.js",
+  "https://cdn.jsdelivr.net/npm/svgpath@2.6.0/+esm",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      );
+    }),
+  );
+});
