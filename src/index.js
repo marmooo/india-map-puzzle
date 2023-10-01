d7f2a581fc75fe3a4de2b7f51df0f600eee07dcf
marmooo <marmooo@users.noreply.github.com>
2023-10-02 08:53:12 +0900

    improve guide position

--- src/index.js
+++ src/index.js
@@ -389,7 +389,7 @@ function setPieceGuideEvent(island, group) {
       const e = event.e;
       const touch = (e instanceof TouchEvent) ? e.touches[0] : e;
       const tx = touch.clientX;
-      const ty = touch.clientY - 30;
+      const ty = touch.clientY - 16;
       const id = getStateId(island);
       const stateName = stateNames[id];
       const html = `
@@ -551,7 +551,7 @@ function findPieceNodes(offsetX, offsetY) {
 
 function setMapGuideTooltip(event, island) {
   const tx = event.clientX;
-  const ty = event.clientY - 30;
+  const ty = event.clientY - 16;
   const id = getStateId(island);
   const stateName = stateNames[id];
   const html = `
